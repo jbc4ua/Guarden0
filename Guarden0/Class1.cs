80c7d918166bd1063fbc05b8ea921d3a02d3475e
jbc4ua <43827554+jbc4ua@users.noreply.github.com>
2020-11-08 16:38:11 -0600

    Add project files.

---
+++ Guarden0/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guarden0
+{
+    class Class1
+    {
+    }
+}
---
+++ Guarden0/Connect Device.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guarden0
+{
+    class Connect_Device
+    {
+        public void Show()
+        {
+
+        }
+
+    }
+}
